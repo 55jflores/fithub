45013dcc7a48b198d025ba9dd37633bfb6766e1a
Jesus Flores <jesus.flores1008@gmail.com>
2023-05-22 16:30:53 -0700

    Responsive iframes

--- src/app/mysplit/legs/page.tsx
+++ src/app/mysplit/legs/page.tsx
@@ -10,71 +10,68 @@ export const metadata: Metadata = {
 
 export default function Legs() {
   return (
-    <>
-<div className='flex flex-col items-center h-screen overflow-y-scroll'>
-<div className='max-w-2xl mx-auto'>
-
-        <p>Leg Extensions</p>
-        <p>Set 1: 15 Set 2: 12 Set 3: 12</p>
-        <iframe
-          className='w-full aspect-video'
-          src="https://www.youtube.com/embed/8Jqof7z3QYM"
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowFullScreen
-        />
-
-        <p>Leg Curls</p>
-        <p>Set 1: 15 Set 2: 12 Set 3: 12</p>
-        <iframe
-          className='w-full aspect-video'
-          src="https://www.youtube.com/embed/fK0uZ3KRZRI"      
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowFullScreen
-        />
-
-        <p>Smith Machine Bulgarian Split Squats</p>
-        <p>Set 1: 15 Set 2: 12 Set 3: 12</p>
-        <iframe
-          className='w-full aspect-video'
-          src="https://www.youtube.com/embed/oLi2QcD0B3M"      
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowFullScreen
-        />
-        
-        <p>Smith Machine Hip Thrust</p>
-        <p>Set 1: 15 Set 2: 12 Set 3: 12</p>
-        <iframe
-          className='w-full aspect-video'
-          src="https://www.youtube.com/embed/s6u3ESE4kMg"      
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowFullScreen
-        />
-        
-        <p>Smith Machine Hack Squat</p>
-        <p>Set 1: 15 Set 2: 12 Set 3: 12</p>
-        <iframe
-          className='w-full aspect-video'
-          src="https://www.youtube.com/embed/i2LRBez5xZY"      
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowFullScreen
-        />
-        
-        <p>Cable Pullthrough</p>
-        <p>Set 1: 15 Set 2: 12 Set 3: 12</p>
-        <iframe
-          className='w-full aspect-video'
-          src="https://www.youtube.com/embed/4oZ_0_bQcOg"      
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowFullScreen
-        />
-      </div>
-      </div>
-    </>
+<div className='flex flex-col items-center min-h-screen px-10 md:px-8 lg:px-12'>
+<div className='flex flex-col max-w-2xl w-full mx-auto my-auto'>
+      <p>Leg Extensions</p>
+      <p>Set 1: 15 Set 2: 12 Set 3: 12</p>
+      <iframe
+      className='w-full h-64 md:h-96 lg:h-128 aspect-w-16 aspect-h-9'
+      src="https://www.youtube.com/embed/8Jqof7z3QYM"
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      />
+  
+      <p>Leg Curls</p>
+      <p>Set 1: 15 Set 2: 12 Set 3: 12</p>
+      <iframe
+      className='w-full h-64 md:h-96 lg:h-128 aspect-w-16 aspect-h-9'
+      src="https://www.youtube.com/embed/fK0uZ3KRZRI"
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      />
+  
+      <p>Smith Machine Bulgarian Split Squats</p>
+      <p>Set 1: 15 Set 2: 12 Set 3: 12</p>
+      <iframe
+      className='w-full h-64 md:h-96 lg:h-128 aspect-w-16 aspect-h-9'
+      src="https://www.youtube.com/embed/oLi2QcD0B3M"
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      />
+  
+      <p>Smith Machine Hip Thrust</p>
+      <p>Set 1: 15 Set 2: 12 Set 3: 12</p>
+      <iframe
+      className='w-full h-64 md:h-96 lg:h-128 aspect-w-16 aspect-h-9'
+      src="https://www.youtube.com/embed/s6u3ESE4kMg"
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      />
+  
+      <p>Smith Machine Hack Squat</p>
+      <p>Set 1: 15 Set 2: 12 Set 3: 12</p>
+      <iframe
+      className='w-full h-64 md:h-96 lg:h-128 aspect-w-16 aspect-h-9'
+      src="https://www.youtube.com/embed/i2LRBez5xZY"
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      />
+  
+      <p>Cable Pullthrough</p>
+      <p>Set 1: 15 Set 2: 12 Set 3: 12</p>
+      <iframe
+      className='w-full h-64 md:h-96 lg:h-128 aspect-w-16 aspect-h-9'
+      src="https://www.youtube.com/embed/4oZ_0_bQcOg"
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      />
+    </div>
+  </div>
   )
 }
